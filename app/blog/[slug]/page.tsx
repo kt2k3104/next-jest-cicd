c3c9fe9dbb3cfcd0916a7b90beda327d1f09b0a8
khai <kt2k3104@gmail.com>
2023-11-24 16:49:33 +0700

    update code 2

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default function Page({ params }: Params) {
   return (
     <>
       <h1>Slug: {params.slug}</h1>
-      <p>Dieu Thuong kkk</p>
+      <p>Dieu Thuong</p>
       <p>Wellcome to Hanoi</p>
     </>
   )
